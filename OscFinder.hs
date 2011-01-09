b91c004ef94b759f7e1dc9557a4ec3366f972dec
Remy Goldschmidt <taktoa@gmail.com>
2011-01-08 22:21:56 -0500

    OscFinder.hs Sat Jan  8 22:21:56 EST 2011

--- OscFinder.hs
+++ OscFinder.hs
@@ -1,72 +1,9 @@
-import Data.List.Split
-import Data.List.Key (sort)
-import Data.List (group)
+
 import Data.Ord
-import Data.Tuple.HT
 import System.IO
-import CPUTime
-
-decompCore :: Double -> Int -> (Double, Int)
-decompCore x y
-        | x >= 10.0                 = decompCore (x / 10.0) (y + 1)
-        | x < 1.0                   = decompCore (x * 10.0) (y - 1)
-        | otherwise                 = (x, y)
-
-decomp x = decompCore x 0
-
-recomp (x, y) = x * (10 ** y)
-
-prefixParse :: Double -> (Int, String)
-prefixParse x = (a, b)
-        where
-        c = (snd (decomp x))
-        a = mod c 3
-        b = case (c - a) of
-                -12 -> "pico"
-                -9 -> "nano"
-                -6 -> "micro"
-                -3 -> "milli"
-                0 -> ""
-                3 -> "kilo"
-                6 -> "mega"
-
-colorParseCore :: Char -> String
-colorParseCore x = case x of
-                    '0' -> "Black"
-                    '1' -> "Brown"
-                    '2' -> "Red"
-                    '3' -> "Orange"
-                    '4' -> "Yellow"
-                    '5' -> "Green"
-                    '6' -> "Blue"
-                    '7' -> "Violet"
-                    '8' -> "Gray"
-                    '9' -> "White"
-
-colorParse :: Double -> String
-colorParse x = a ++ " " ++ b ++ " " ++ c
-        where
-        m = decomp x
-        n = colorParseCore (head (show (snd m - 1)))
-        o = round (10 * fst m)
-        (p, q) = ((show o) !! 0, (show o) !! 1)
-        (r, s) = (colorParseCore p, colorParseCore q)
-        (a, b, c) = (r, s, n)
-
-makeList :: (Double, Double) -> [Int] -> [Double]
-makeList range vals = if low < high then map (* low) values ++ makeList (low * 10, high) vals else []
-        where
-        low = fst range
-        high = snd range
-        values = map fromIntegral vals
-
-makeSet :: [[Double]] -> ([Double] -> (Double, [Double])) -> Double -> [(Double, [Double])]
-makeSet x f freq = head . group . sort fst $ map ((\ b -> (abs (fst b - freq), snd b)) . f) (sequence x)
-
-findBest :: [(Double, Double)] -> [Int] -> ([Double] -> (Double, [Double])) -> Double -> [(Double, [Double])]
-findBest ranges preclist = makeSet (zipWith makeList ranges (replicate (length ranges) preclist))
-
-findBest' = makeSet
+import System.CPUTime
+import Parse
+import Optimize
 
 phaseShift :: [Double] -> (Double, [Double])
 phaseShift x = (1/(15.390597961942367 * head x * (x !! 1)), x)
